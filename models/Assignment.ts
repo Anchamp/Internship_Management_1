c843105db5300b1a977da4b5d02f498666a7c7c7
Rishi <rishitezreddy@gmail.com>
2025-06-25 10:36:56 +0530

    Assignment Model Created

---
+++ models/Assignment.ts
@@ -0,0 +1,58 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const assignmentSchema = new Schema({
+    assignmentName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    assignmentFrom: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    deadline: Date,
+    status: {
+      type: String,
+      enum: ['pending', 'active', 'review', 'completed'],
+      required: true,
+      default: 'pending',
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    assignedTo: [{
+      type: String,
+      default: 'none',
+    }],
+    organizationName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    organizationId: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {strict: false}
+);
+
+assignmentSchema.pre("updateOne", function () {
+  this.set({ updatedAt: new Date() });
+});
+
+const Assignment = models.Assignment || mongoose.model("Assignment", assignmentSchema);
+
+export default Assignment;
+
