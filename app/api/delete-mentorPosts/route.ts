83eaabacd51973f904ba5bd5464ec76b2404797e
Rishi <rishitezreddy@gmail.com>
2025-07-15 12:46:22 +0530

    Delete Mentor Post logic built

---
+++ app/api/delete-mentorPosts/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from 'next/server';
+import dbConnect from '@/lib/mongoose';
+import User from '@/models/User';
+import Team from '@/models/Team';
+import MentorPost from '@/models/MentorPost';
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+
+    const body = await request.json();
+    const { username, postId } = body;
+
+    if (!username || !postId) {
+      return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+    }
+
+    const post = await MentorPost.findById(postId).lean();
+    if (!post) {
+      return NextResponse.json({ error: 'Post not found' }, { status: 404 });
+    }
+
+    const user = await User.findOne({ username }).select("username").lean();
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    const adminUser = await User.findOne({ role: 'admin', organizationName: post.organizationName, organizationId: post.organizationId}).select("username").lean();
+
+    if (!adminUser) {
+      return NextResponse.json({ error: 'Admin user not found for this organization' }, { status: 404 });
+    }
+
+    if (post.postedBy !== username && user.username !== adminUser.username) {
+      return NextResponse.json({ error: 'Unauthorized Access' }, { status: 403 });
+    }
+
+    // Delete the post
+    const response = await MentorPost.findByIdAndDelete(postId);
+    if (!response) {
+      return NextResponse.json({ error: 'Failed to delete post' }, { status: 500 });
+    }
+
+    return NextResponse.json({ message: 'Post deleted successfully' }, { status: 200 });
+
+  } catch (error: any) {
+    console.error('Error in POST /mentor-posts:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
